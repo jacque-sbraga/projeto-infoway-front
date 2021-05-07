649dadcb37c9a919ce083551ab7b00e4afa5875d
Catia Yamamura <cayamamura@gmail.com>
2021-05-07 00:28:39 -0300

    Instalação do Angular Material, implementação de categorias e rotas na header, e config para aceitar null

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,97 @@
+import { NgModule } from '@angular/core';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  imports: [
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatListModule,
+    MatDatepickerModule,        
+    MatNativeDateModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDialogModule,      
+    MatCheckboxModule,                
+    MatTabsModule,        
+    MatExpansionModule,
+    MatRadioModule,        
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatBottomSheetModule,
+    MatBadgeModule,
+    MatGridListModule,        
+    MatChipsModule,
+    MatSlideToggleModule,        
+    MatTooltipModule,        
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+  ],
+  exports: [
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatListModule,
+    MatDatepickerModule,        
+    MatNativeDateModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDialogModule,        
+    MatCheckboxModule,        
+    MatTabsModule,        
+    MatExpansionModule,
+    MatRadioModule,        
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatBottomSheetModule,
+    MatBadgeModule,
+    MatGridListModule,        
+    MatChipsModule,
+    MatSlideToggleModule,        
+    MatTooltipModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+  ]
+})
+export class MaterialModule { }
