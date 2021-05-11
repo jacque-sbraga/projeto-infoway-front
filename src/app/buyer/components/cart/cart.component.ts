f5471daefd3bb565d340948e2c3e4c69a3b752b8
mayra medeiros <mayramedeiros83@gmail.com>
2021-05-11 16:04:06 -0300

    teste

--- src/app/buyer/components/cart/cart.component.ts
+++ src/app/buyer/components/cart/cart.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  counter: number = 1;
+  
+  setCounterPlus() {
+    this.counter += 1
+  }
+
+  setCounterMinus() {
+    if (this.counter > 1) {
+      this.counter -= 1
+    }
+  }
 
   constructor() { }
 
