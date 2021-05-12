c561ad336697e161971835dcdb2a7ffd80a9592e
Natasha Gulyas <natashagulyas@gmail.com>
2021-05-11 23:40:09 -0300

    HTML e CSS User register ok

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,2 +1,12 @@
 export class Users {
+  id: number;
+  name: string;
+  surname: number;
+  cpf: number;
+  ddd: number;
+  phone: number;
+  login: string;
+  password: string; //virtual
+  password_hash: string;
+  provider: boolean;
 }
