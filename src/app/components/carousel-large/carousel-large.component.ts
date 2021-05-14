7df217f7b0f82238d58cbb60827263b81754ee11
felipebelarmino <epilefgomes1@gmail.com>
2021-05-13 22:14:10 -0300

    Fix bux login menu, logged as role user ok

--- src/app/components/carousel-large/carousel-large.component.ts
+++ src/app/components/carousel-large/carousel-large.component.ts
@@ -1,55 +1,18 @@
-import { Component, ViewChild } from '@angular/core';
-
-import {
-  NgbCarousel,
-  NgbSlideEvent,
-  NgbSlideEventSource,
-} from '@ng-bootstrap/ng-bootstrap';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-carousel-large',
   templateUrl: './carousel-large.component.html',
   // styleUrls: ['./carousel-large.component.css'],
 })
 export class CarouselLargeComponent {
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  images = [
+    'https://www.fastshop.com.br/wcsstore/FastShop/img/home/2021/05/10/intpro-_-100521-_-99-_-6869252683-_-Acer-_-IN-_-Grupo-_-rotativohome.jpg',
+    'https://www.fastshop.com.br/wcsstore/FastShop/img/home/2021/05/10/intpro-_-100521-_-05-_-SGQN55LS03TA_PRD-_-Samsung-_-VD-_-Grupo-_-rotativohome.jpg',
+    'https://www.fastshop.com.br/wcsstore/FastShop/img/home/2021/05/10/intpro-_-110521-_-06-_-ND-_-ND-_-CL-_-Grupo-_-rotativohome.jpg',
+    'https://www.fastshop.com.br/wcsstore/FastShop/img/home/2021/05/10/intpro-_-100521-_-99-_-L281TR0003BRB-_-Lenovo-_-IN-_-Grupo-_-rotativohome.jpg',
+    'https://www.fastshop.com.br/wcsstore/FastShop/img/home/2021/05/10/intpro-_-100521-_-15-_-ND-_-JBL-_-AD-_-Produto-_-rotativohome.jpg',
+    'https://www.fastshop.com.br/wcsstore/FastShop/img/home/2021/05/10/intpro-_-110521-_-00-_-MassEffect-_-ND-_-GM-_-Grupo-_-rotativohome.jpg',
+    'https://www.fastshop.com.br/wcsstore/FastShop/img/home/2021/05/10/intpro-_-130521-_-00-_-ND-_-LG-_-VD-_-Grupo-_-rotativohome.jpg',
+  ];
 
-  // images = [62, 83, 466, 965, 982, 1043, 738].map(
-  //   (n) => `https://picsum.photos/id/${n}/900/500`
-  // );
-
-  // paused = false;
-  // unpauseOnArrow = false;
-  // pauseOnIndicator = false;
-  // pauseOnHover = true;
-  // pauseOnFocus = true;
-
-  // @ViewChild('carousel', { static: true }) carousel: NgbCarousel;
-
-  // togglePaused() {
-  //   if (this.paused) {
-  //     this.carousel.cycle();
-  //   } else {
-  //     this.carousel.pause();
-  //   }
-  //   this.paused = !this.paused;
-  // }
-
-  // onSlide(slideEvent: NgbSlideEvent) {
-  //   if (
-  //     this.unpauseOnArrow &&
-  //     slideEvent.paused &&
-  //     (slideEvent.source === NgbSlideEventSource.ARROW_LEFT ||
-  //       slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
-  //   ) {
-  //     this.togglePaused();
-  //   }
-  //   if (
-  //     this.pauseOnIndicator &&
-  //     !slideEvent.paused &&
-  //     slideEvent.source === NgbSlideEventSource.INDICATOR
-  //   ) {
-  //     this.togglePaused();
-  //   }
-  // }
 }
