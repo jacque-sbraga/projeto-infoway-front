1fecac6c207ee7c46df47e66a3b924a29d47ccb4
Jacqueline Santos <jaquelinesantosbraga@gmail.com>
2021-05-13 23:54:55 -0300

    refatarado o footer

--- src/app/components/carousel-large/carousel-large.component.ts
+++ src/app/components/carousel-large/carousel-large.component.ts
@@ -12,7 +12,13 @@ import {
   // styleUrls: ['./carousel-large.component.css'],
 })
 export class CarouselLargeComponent {
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  images = [
+    "https://www.fastshop.com.br/wcsstore/FastShop/img/home/2021/05/10/intpro-_-100521-_-01-_-super-oportunidades-_-ND-_-ND-_-Grupo-_-rotativohome.jpg",
+    "https://www.fastshop.com.br/wcsstore/FastShop/img/home/2021/05/10/intpro-_-100521-_-02-_-FastPrime-_-ND-_-ND-_-ND-_-rotativohome.jpg",
+    "https://www.fastshop.com.br/wcsstore/FastShop/img/home/2021/05/10/intpro-_-100521-_-99-_-6869252683-_-Acer-_-IN-_-Grupo-_-rotativohome.jpg",
+    "https://www.fastshop.com.br/wcsstore/FastShop/img/home/2021/05/10/intpro-_-100521-_-05-_-SGQN55LS03TA_PRD-_-Samsung-_-VD-_-Grupo-_-rotativohome.jpg",
+    "https://www.fastshop.com.br/wcsstore/FastShop/img/home/2021/05/10/intpro-_-110521-_-06-_-ND-_-ND-_-CL-_-Grupo-_-rotativohome.jpg"
+  ]
 
   // images = [62, 83, 466, 965, 982, 1043, 738].map(
   //   (n) => `https://picsum.photos/id/${n}/900/500`
